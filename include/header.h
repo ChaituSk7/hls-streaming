b5351caac9e02cec1b7375698cf529222e110c58
ChaituSk7 <bskchaitanya955@gmail.com>
2023-06-02 18:38:14 +0530

    created header file

---
+++ include/header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <gst/gst.h>
+
+typedef struct _HlsStreamData {
+    GstElement *video_source;
+    GstElement *video_convert;
+    GstElement *clock_overlay;
+    GstElement *video_scale;
+    GstElement *caps_filter1;
+    GstElement *video_encoder;
+    GstElement *caps_filter2;
+    GstElement *video_mux;
+    GstElement *hls_sink;
+    GMainLoop *loop;
+} HlsStreamData;
+
+void hls_pipeline ();
+
+#endif
